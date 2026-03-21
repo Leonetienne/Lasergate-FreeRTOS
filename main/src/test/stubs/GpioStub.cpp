00dc078d8205c75f0a3906fa654f5f0f8e8577e6
Leon Etienne <github@leonetienne.de>
2026-03-21 20:05:58 +0100

    feat: add interrogators for unit tests to GpioStub

--- main/src/test/stubs/GpioStub.cpp
+++ main/src/test/stubs/GpioStub.cpp
@@ -5,13 +5,29 @@
 #include "test/stubs/GpioStub.h"
 
 esp_err_t GpioStub::gpioResetPin(gpio_num_t pinNum) noexcept {
-    return 0;
+    return ESP_OK;
 }
 
 esp_err_t GpioStub::gpioSetDirection(gpio_num_t pinNum, gpio_mode_t pinMode) noexcept {
-    return 0;
+    pinDirectionMap[pinNum] = pinMode;
+    return ESP_OK;
 }
 
 esp_err_t GpioStub::gpioSetlevel(gpio_num_t pinNum, uint32_t level) noexcept {
-    return 0;
+    pinLevelMap[pinNum] = level;
+    return ESP_OK;
+}
+
+gpio_num_t GpioStub::test_gpioGetMode(gpio_num_t pinNum) noexcept {
+    if (!pinDirectionMap.contains(pinNum)) {
+        return GPIO_MODE_DISABLE;
+    }
+    return pinDirectionMap[pinNum];
+}
+
+uint32_t GpioStub::test_gpioGetLevel(gpio_num_t pinNum) noexcept {
+    if (!pinLevelMap.contains(pinNum)) {
+        return 0;
+    }
+    return pinLevelMap[pinNum];
 }
