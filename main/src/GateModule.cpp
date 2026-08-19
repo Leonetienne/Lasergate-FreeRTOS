d4d7cd03a731a26e9b7704a60d82490f3c7cbb34
Leon Etienne <github@leonetienne.de>
2026-08-19 22:23:45 +0200

    feat: simple gate module class

--- main/src/GateModule.cpp
+++ main/src/GateModule.cpp
@@ -3,10 +3,12 @@
 constexpr int INITIAL_LDR_THRESH = 0;
 
 GateModule::GateModule(
+    StateMachine &stateMachine,
     LaserDiode &laserDiode,
     LightEmittingDiode &statusLed,
     LaserSensor &laserSensor
 ) noexcept:
+    stateMachine {stateMachine},
     laserDiode {laserDiode},
     statusLed {statusLed},
     laserSensor {laserSensor}
@@ -69,4 +71,114 @@ bool GateModule::isReady() const noexcept {
 }
 
 void GateModule::fixedUpdate() {
+    switch (stateMachine.getState()) {
+        case STATE::FAULT:
+            updateStateFault();
+            break;
+        case STATE::USER_ADJUSTING_BEAMS:
+            updateStateUserAdjustingBeams();
+            break;
+        case STATE::CALIBRATION_LDR_THRESH:
+            updateStateCalibrationLdrThresh();
+            break;
+        case STATE::CALIBRATION_MODULATION_FREQUENCY:
+            updateStateCalibrationModulationFrequency();
+            break;
+        case STATE::OBSERVING:
+            updateStateObserving();
+            break;
+        case STATE::DISARMED:
+            updateStateDisarmed();
+
+        break;
+        default:
+            // Shouldn't happen
+            stateMachine.setState(STATE::FAULT, "GateModule::fixedUpdate: unhandled state");
+            break;
+    }
+}
+
+void GateModule::onStateChange() {
+    switch (stateMachine.getState()) {
+        case STATE::FAULT:
+            onStateFault();
+            break;
+        case STATE::USER_ADJUSTING_BEAMS:
+            onStateUserAdjustingBeams();
+            break;
+        case STATE::CALIBRATION_LDR_THRESH:
+            onStateCalibrationLdrThresh();
+            break;
+        case STATE::CALIBRATION_MODULATION_FREQUENCY:
+            onStateCalibrationModulationFrequency();
+            break;
+        case STATE::OBSERVING:
+            onStateObserving();
+            break;
+        case STATE::DISARMED:
+            onStateDisarmed();
+
+            break;
+        default: break; // Shouldn't happen
+    }
+}
+
+void GateModule::onStateFault() noexcept {
+    if (!isInitialized) return;
+    laserDiode.turnOff();
+    statusLed.turnOff();
+}
+
+void GateModule::onStateUserAdjustingBeams() noexcept {
+    if (!isInitialized) return;
+    if (!laserDiode.turnOn() || !statusLed.turnOff()) {
+        stateMachine.setState(STATE::FAULT, "GateModule::onStateUserAdjustingBeams: failed to turn off laser diode / status led");
+    }
+}
+
+void GateModule::onStateCalibrationLdrThresh() noexcept {
+    if (!isInitialized) return;
+}
+
+void GateModule::onStateCalibrationModulationFrequency() noexcept {
+    if (!isInitialized) return;
+}
+
+void GateModule::onStateObserving() noexcept {
+    if (!isInitialized) return;
+}
+
+void GateModule::onStateAlarm() noexcept {
+    if (!isInitialized) return;
+}
+
+void GateModule::onStateDisarmed() noexcept {
+    if (!isInitialized) return;
+}
+
+void GateModule::updateStateFault() noexcept {
+    if (!isInitialized) return;
+}
+
+void GateModule::updateStateUserAdjustingBeams() noexcept {
+    if (!isInitialized) return;
+}
+
+void GateModule::updateStateCalibrationLdrThresh() noexcept {
+    if (!isInitialized) return;
+}
+
+void GateModule::updateStateCalibrationModulationFrequency() noexcept {
+}
+
+void GateModule::updateStateObserving() noexcept {
+    if (!isInitialized) return;
+}
+
+void GateModule::updateStateAlarm() noexcept {
+    if (!isInitialized) return;
+}
+
+void GateModule::updateStateDisarmed() noexcept {
+    if (!isInitialized) return;
 }
